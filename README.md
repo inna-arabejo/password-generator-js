5906aab3601aefeb9b8b3614c4d5c6723aa2aed4
Anne Saber <iarabejo3@gmail.com>
2022-03-05 00:51:51 -0600

    Added description, screenshot, and links to readme file. Added detailed comments in javascript file.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# password-generator-js
+# Javascript Password Generator 
+
+## Description
+Create a password generator that secures access to sensitive data. Generate a password that meets these certain criteria. 
+
+## Task
+To generate a new password, the following requirements are met when:
+- A prompt of the password length is displayed after clicking on the generate password button.
+- Questions about whether to include an uppercase, lowercase, numeric, and/or special characters are confirmed.
+- The password generator generates a new password after selecting all the prompts.
+- The password is displayed in the page.
+
+## Screenshot
+![Password generator screenshot](./images/password-generator.jpg)
+
+## Link to the Final Page
+[Link to the password generator web page]https://inna-arabejo.github.io/password-generator-js/
+
+## Sources
+- https://dev.to/code_mystery/random-password-generator-using-javascript-6a
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
--- script.js
+++ script.js
@@ -3,81 +3,64 @@
 var generateBtn = document.querySelector("#generate");
 
 // Provided by instructor
-var allowUppercase;
+var allowUppercase; //variables 
 var allowLowerCase;
 var allowNumbers;
 var allowSpecials;
 var passwordCharacterCount;
 
-var uppercaseChars = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+var uppercaseChars = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]; //listed variables
 var lowercaseChars = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
 var numberChars = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
 var specialChars = [ "!", "&", "@", "~", "^", "#" ];
-
-// Provided by instructor
-
-
-
-
-
 
 
 // Repeat for other criteria; for the number of characters, use a prompt statement. Google it. NOTE: prompt statements think any value you provide is a string. You will need to convert it to a number.
 
 // Once all the criteria are determined, this function will generate the password. You can create other functions also if you need them.
-function generatePassword(){
+
+
+// Provided by instructor
+function generatePassword(){ //a block of code designed to generate password
   var randomChar = [];
-  var finalResult = "";
+  var finalResult = "";  //shows final result of password after completing prompts
   
   
   function askAboutPasswordCharCount(){
-    passwordCharacterCount = window.prompt("Input a password length between 8 and 128 characters. ");
-    return passwordCharacterCount;
+    passwordCharacterCount = window.prompt("Input a password length between 8 and 128 characters. "); //Asks the question 
+    return passwordCharacterCount; 
   }
-    askAboutPasswordCharCount();
-
-    // var charValidation = false;
-    // if ((passwordCharacterCount <  8) || (passwordCharacterCount > 128))
-    // alert("Password length must be between 8 and 128 characters. ");
-    // else if ((!askAboutUppercase)&&(!askAboutLowerCase)&&(!askAboutNumbers)&&(!askAboutSpecials))
-    // alert("No password generated.");
-    // charValidation = true;
+    askAboutPasswordCharCount(); //displays question prompt
     
 
-
-  
-    function askAboutUppercase(){
-    allowUppercase = confirm("Are upper case characters allowed?");
+  function askAboutUppercase(){
+    allowUppercase = confirm("Are upper case characters allowed?"); 
     console.log(allowUppercase);
   }
-  askAboutUppercase(); // ask the questions first
-  
-
-  if (allowUppercase){
-    randomChar.push(uppercaseChars);
+  askAboutUppercase(); 
+  if (allowUppercase){  //if statement
+    randomChar.push(uppercaseChars.join("")); //creates and returns new string by concatenating all of the elements
   }
   
   
-  function askAboutLowerCase(){
-    allowLowerCase = confirm("Are lower case characters allowed?");
+  function askAboutLowerCase(){ 
+    allowLowerCase = confirm("Are lower case characters allowed?"); //code inside curly brackets is code to be executed
     console.log(allowLowerCase);
   }
   askAboutLowerCase();
   if (allowLowerCase){
-    randomChar.push(lowercaseChars);
+    randomChar.push(lowercaseChars.join(""));
   }
-  
   
   
   function askAboutNumbers(){
     allowNumbers = confirm("Are number characters allowed?");
-    console.log(allowNumbers);
+    console.log(allowNumbers); //console.log method outputs a message to the web console
   }
   askAboutNumbers();
   if (allowNumbers){
-    randomChar.push(numberChars);
+    randomChar.push(numberChars.join(""));
   }
-  
   
   
   function askAboutSpecials(){
@@ -85,26 +68,21 @@ function generatePassword(){
     console.log(allowSpecials);
   }
   askAboutSpecials();
-  if (allowSpecials){
-    randomChar.push(specialChars);
+  if (allowSpecials){ //if statement used to specify a block of code to be executed, if true
+    randomChar.push(specialChars.join(""));
   }
-  
-  
   
   
   const newCharArray = randomChar.toString();
   
   
-  for (var i=1; i<=passwordCharacterCount; i++){
+  for (var i=1; i<=passwordCharacterCount; i++){ //for loop used to execute a block of code a number of times
     var randomIdx = Math.floor(Math.random() * newCharArray.length);
     finalResult += newCharArray[randomIdx];
   }
 
-  return finalResult;
+  return finalResult;  //ends the function execution and specifies a value to be returned to the function
 }
-
-
-
 
 // HINT: Remember that a for-loop can iterate from a starting number to a ending number, such as the number of characters in a password.
 
@@ -115,7 +93,7 @@ function generatePassword(){
 function writePassword() {
   
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password"); //returns the first element in the document that matches the specified set of CSS selectors
   passwordText.value = password;
 }
 
